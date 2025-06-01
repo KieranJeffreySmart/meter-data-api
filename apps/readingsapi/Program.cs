31826b68fa11085e7b3884598b4c739c5fd263c1
Kieran Smart <kieransmart77@googlemail.com>
2025-06-01 15:34:44 +0000

    Make single reading test pass refactor empty data test to use multipart content

--- apps/readingsapi/Program.cs
+++ apps/readingsapi/Program.cs
@@ -1,4 +1,7 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 public class Program
 {
@@ -9,6 +12,11 @@ public class Program
         // Add services to the container.
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
+        builder.Services.AddDbContext<MeterReadingsContext>(
+            options =>
+            options.UseInMemoryDatabase(Guid.NewGuid().ToString())
+            );
+
 
         var app = builder.Build();
 
@@ -20,12 +28,67 @@ public class Program
 
         app.UseHttpsRedirection();
 
-        app.MapPost("/meter-reading-uploads", () =>
+        _ = app.MapPost("/meter-reading-uploads", async (HttpRequest request, [FromServices] MeterReadingsContext dbContext) =>
         {
-            return Results.BadRequest("Bad Request: No data provided.");
+            await dbContext.Database.EnsureCreatedAsync();
+
+            try
+            {
+                var formCollection = await request.ReadFormAsync();
+                if (formCollection == null || formCollection.Files.Count == 0)
+                {
+                    // If no files are uploaded, return a Bad Request response
+                    return Results.BadRequest("Bad Request: Invalid data provided.");
+                }
+
+                foreach (var file in formCollection.Files)
+                {
+                    if (file.Length == 0)
+                    {
+                        continue; // Skip empty files
+                    }
+
+                    using var reader = new StreamReader(file.OpenReadStream());
+                    string? line;
+                    while ((line = await reader.ReadLineAsync()) != null)
+                    {
+                        var parts = line.Split(',');
+                        DateTimeFormatInfo dtfi = new DateTimeFormatInfo
+                        {
+                            ShortDatePattern = "dd/MM/yyyy",
+                            LongDatePattern = "dd/MM/yyyy HH:mm"
+                        };
+
+                        var reading = new Reading(Guid.NewGuid(), Convert.ToInt32(parts[0]), Convert.ToDateTime(parts[1], dtfi), Convert.ToInt32(parts[2]));
+                        dbContext.Readings.Add(reading);
+                    }
+                }
+                
+                await dbContext.SaveChangesAsync();
+            }
+            catch (InvalidDataException _)
+            {
+                return Results.BadRequest("Bad Request: Invalid data provided.");
+            }
+
+            return Results.Ok("1");
         })
         .WithName("MeterReadingUploads");
 
         app.Run();
     }
 }
+
+public class MeterReadingsContext : DbContext
+{
+    public MeterReadingsContext(DbContextOptions<MeterReadingsContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Reading> Readings { get; set; }
+}
+
+public record Account(int AccountId, string FirstName, string LastName);
+
+public record Reading(Guid ReadingId, int AccountId, DateTime MeterReadingDateTime, int MeterReadValue);
--- apps/readingsapi_tests/EndToEndTests.cs
+++ apps/readingsapi_tests/EndToEndTests.cs
@@ -1,9 +1,15 @@
 using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 
 namespace readingsapi_tests;
 
-public class EndToEndTests: IClassFixture<WebApplicationFactory<Program>>
+public class EndToEndTests : IClassFixture<WebApplicationFactory<Program>>
 {
     // As an Energy Company Account Manager, 
     // I want to be able to load a CSV file of Customer Meter Readings 
@@ -49,6 +55,11 @@ public class EndToEndTests: IClassFixture<WebApplicationFactory<Program>>
         response.EnsureSuccessStatusCode();
         var responseData = await response.Content.ReadAsStringAsync();
         Assert.Contains("10", responseData); // TODO: Adjust based on actual valid data from file
+
+        // And I can see all the data was persisted
+        // Note: This step is not actusally part of the acceptance criteria and I could use flat files
+        //       however the exercise requires that the data is persisted to a DB and so I have added this to the test
+        // TODO: Figure out the easiest way to get persisted data
     }
 
     [Fact]
@@ -56,33 +67,70 @@ public class EndToEndTests: IClassFixture<WebApplicationFactory<Program>>
     {
         // When I submit empty data
         var client = _factory.CreateClient();
-        var content = new StringContent(string.Empty);
+        var content = new MultipartFormDataContent {  };
         var response = await client.PostAsync("/meter-reading-uploads", content);
 
         // Then I should be informed my request was bad
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         var responseData = await response.Content.ReadAsStringAsync();
-        Assert.Contains("Bad Request: No data provided.", responseData);
+        Assert.Contains("\"Bad Request: Invalid data provided.\"", responseData);
     }
 
     [Fact]
     public async Task SubmitSingleValidReadingForSingleCustomer()
     {
         // Given I have a customer account
-        var accountData = "2344,Tommy,Test";
-        //TODO: seed accounts data into the database
+        string localDbName = string.Empty;
+        var localWebFactory = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(s =>
+            {
+                s.AddDbContext<MeterReadingsContext>(options =>
+                {
+                    localDbName = "TestDB_" + Guid.NewGuid().ToString();
+                    options.UseInMemoryDatabase(localDbName)
+                    .UseAsyncSeeding(async (context, _, camcellationToken) =>
+                    {
+                        
+                        (context as MeterReadingsContext)?.Accounts.Add(new Account(1002, "John", "Doe"));
+                        await context.SaveChangesAsync(camcellationToken);
+                    });
+                });
+            });
+        });
 
-        // And I have some meter reading data
+        // And I have a single entry of meter reading data
         var readingsData = "2344,22/04/2019 09:24,1002,";
 
         // When I submit the data
-        var client = _factory.CreateClient();
-        var content = new StringContent(readingsData);
+        var client = localWebFactory.CreateClient();
+        var content = CreateFakeMultiPartFormData(readingsData);
         var response = await client.PostAsync("/meter-reading-uploads", content);
 
-        // Then I should be informed all readings were successfully submitted
+        // Then I should be informed the reading was successfully submitted
         response.EnsureSuccessStatusCode();
         var responseData = await response.Content.ReadAsStringAsync();
         Assert.Contains("1", responseData);
+
+        // And I can see the reading was persisted
+        Assert.NotNull(localDbName);
+        Assert.NotEqual(string.Empty, localDbName);
+        var localContext = new MeterReadingsContext(
+            new DbContextOptionsBuilder<MeterReadingsContext>()
+                .UseInMemoryDatabase(localDbName)
+                .Options);
+        Assert.Equal(1, localContext.Readings.Count());
+        var reading = localContext.Readings.First();
+        Assert.Equal(2344, reading.AccountId);
+        Assert.Equal(new DateTime(2019, 4, 22, 9, 24, 0), reading.MeterReadingDateTime);
+        Assert.Equal(1002, reading.MeterReadValue);
+    }
+    
+    private static MultipartFormDataContent CreateFakeMultiPartFormData(string content)
+    {
+        ByteArrayContent byteContent = new ByteArrayContent(Encoding.UTF8.GetBytes(content));
+
+        MultipartFormDataContent multipartContent = new MultipartFormDataContent { {byteContent, "file", "readings.csv"} };
+        return multipartContent;
     }
 }
