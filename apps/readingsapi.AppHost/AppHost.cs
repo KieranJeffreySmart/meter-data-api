f85af8a2ee95c4eb7c4133e427770a1502bc9a03
Kieran Smart <kieransmart77@googlemail.com>
2025-06-04 12:48:55 +0000

    Add basic parts for db and first migration

--- apps/readingsapi.AppHost/AppHost.cs
+++ apps/readingsapi.AppHost/AppHost.cs
@@ -1,10 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres");
+// TODO: Get passwords from a vault or secure store
+var postgressPassword = builder.AddParameter("PostgresPassword", secret: true);
+var postgres = builder.AddPostgres("postgres", password: postgressPassword)
+    .WithDataVolume("postgres_data");
+
 var postgresdb = postgres.AddDatabase("postgresdb");
 
 builder.AddProject<Projects.readingsapi>("readingsapi")
     .WithExternalHttpEndpoints()
-    .WithReference(postgresdb);
+    .WithReference(postgresdb)
+    .WithEnvironment("DB_CONNECTION_TYPE", "postgresdb");
 
 builder.Build().Run();
